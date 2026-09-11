f33e62a4deea60d6056aa4df28a905b2a4afc7d2
f5-huy <timofejbodagin760@gmail.com>
2026-09-11 21:22:14 +0700

    Create AbilityListener.java

---
+++ AbilityListener.java
@@ -0,0 +1,10 @@
+package com.server.rpg.listeners;
+
+import com.server.rpg.RPGPlugin;
+import org.bukkit.event.Listener;
+
+public class AbilityListener implements Listener {
+    private final RPGPlugin plugin;
+    public AbilityListener(RPGPlugin plugin) { this.plugin = plugin; }
+    // Способности классов допишем в модуле №2
+}
